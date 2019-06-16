b2079a2395212cca2e770cf35dbc9d849be96ac0
Gennaro Anzalone <g.anzalone12@gmail.com>
2019-06-16 16:31:22 -0700

    Implement showAlert method call

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ searchUser.addEventListener('keyup', (e) => {
       .then(data => {
         if(data.profile.message == 'Not Found') {
           // Show Alert
-
+          ui.showAlert('User Not Found', 'alert alert-danger');
         } else {
           // Show profile 
           ui.showProfile(data.profile);
