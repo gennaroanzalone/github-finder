a325a2713ad5a71540353e0b8d9a786bf356c0af
Gennaro Anzalone <g.anzalone12@gmail.com>
2019-06-16 19:34:49 -0700

    Add repos count and sort to constructor and fetch call for repos.

--- github.js
+++ github.js
@@ -2,16 +2,23 @@ class GitHub {
   constructor() {
     this.client_id = '20a21b93e85dbbc583c0';
     this.client_secret = '304584ac04383575afea348b59563c53d4c26a6f';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   // Get User Method
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
